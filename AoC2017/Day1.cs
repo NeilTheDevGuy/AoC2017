0157c50d68f5ddaec6b840288ce4d02f80df8d89
ndrinkall <ndrinkall@travelrepublic.co.uk>
2017-12-01 14:02:09 +0000

    Day 1

---
+++ AoC2017/Day1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Aoc2017
+{
+    internal static class Day1
+    {
+        internal static void Run()
+        {
+            using (var reader = File.OpenText(@"inputs\day1.txt"))
+            {
+                var inputDigits = reader.ReadToEnd();
+                WriteSum(inputDigits, 1);
+                WriteSum(inputDigits, inputDigits.Length / 2);
+            }
+            Console.Read();
+        }
+        private static void WriteSum(string input, int offset)
+        {
+            var sum = 0;
+
+            for (var i = 0; i <= input.Length - 1; i++)
+            {
+                var digitToCompare = i + offset < input.Length ? i + offset : i + offset - input.Length;
+                if (input[i] == input[digitToCompare])
+                {
+                    sum += int.Parse(input[i].ToString());
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ AoC2017/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoc2017
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Day1.Run();
+        }
+    }
+}
