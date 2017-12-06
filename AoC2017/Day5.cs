e6dc9f97bce9df74164d1e6877c8b3d3cd15fba6
ndrinkall <ndrinkall@travelrepublic.co.uk>
2017-12-06 09:36:39 +0000

    Day 5

---
+++ AoC2017/Day5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Aoc2017
+{
+    internal static class Day5
+    {
+        internal static void Run()
+        {
+            FindExit(i => i + 1); //376976
+            FindExit(i => i >= 3 ? i - 1 : i + 1); //29227751
+            Console.Read();
+        }
+        private static void FindExit(Func<int, int> incrementor)
+        {
+            using (var reader = File.OpenText(@"inputs\day5.txt"))
+            {
+                var count = 0;
+                var input = reader.ReadToEnd().Split("\n");
+                var index = 0;
+                while (true)
+                {
+                    count++;
+                    var instruction = int.Parse(input[index]);
+                    var alteredInstruction = incrementor(instruction);
+                    input[index] = alteredInstruction.ToString();
+                    index += instruction;
+                    if (index >= input.Length)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
