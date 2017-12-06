48e3b489bb6205ba6ed5f7b48d5fe55685017944
ndrinkall <ndrinkall@travelrepublic.co.uk>
2017-12-06 15:42:00 +0000

    Day 3

--- AoC2017/Day3.cs
+++ AoC2017/Day3.cs
@@ -7,111 +7,219 @@ namespace Aoc2017
 {
     internal static class Day3
     {
-        private static Dictionary<string,int> _partOneGrid = new Dictionary<string, int>();
-        private static Dictionary<string, int> _partTwoGrid = new Dictionary<string, int>();
         public static void Run()
         {
-            PartOne();
+            PartOne(); //480
+            PartTwo(); //349975
             Console.Read();
+        }
+
+        private static void PartTwo()
+        {
+            var gridSize = 347991;
+            var grid = new Dictionary<string, int>();
+            var moveOutcome = new MoveOutcome
+            {
+                X = 0,
+                Y = 0,
+                Direction = Move.First
+            };
+
+            for (var i = 1; i <= gridSize; i++)
+            {
+                moveOutcome = GetNextCoordinates(moveOutcome, grid);
+                var sum = GetSumOfNeighbours(moveOutcome, grid);
+                if (sum > gridSize)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+                grid.Add(GetCoordinate(moveOutcome.X, moveOutcome.Y), sum);
+            }
+        }
+
+        private static int GetSumOfNeighbours(MoveOutcome moveOutcome, Dictionary<string, int> grid)
+        {
+            //Check everything around (if there).
+            var sum = 0;
+            if (moveOutcome.X == 0 && moveOutcome.Y == 0)
+            {
+                //First move, return 1
+                return 1;
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X + 1, moveOutcome.Y)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X + 1, moveOutcome.Y)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X + 1, moveOutcome.Y + 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X + 1, moveOutcome.Y + 1)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X, moveOutcome.Y + 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X, moveOutcome.Y + 1)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X - 1, moveOutcome.Y + 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X - 1, moveOutcome.Y + 1)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X - 1, moveOutcome.Y)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X - 1, moveOutcome.Y)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X - 1, moveOutcome.Y - 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X - 1, moveOutcome.Y - 1)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X, moveOutcome.Y - 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X, moveOutcome.Y - 1)];
+            }
+            if (grid.ContainsKey(GetCoordinate(moveOutcome.X + 1, moveOutcome.Y - 1)))
+            {
+                sum += grid[GetCoordinate(moveOutcome.X + 1, moveOutcome.Y - 1)];
+            }
+            return sum;
         }
 
         private static void PartOne()
         {
-            var arraySize = 23;
-            var currentCoordinates = "0,1";
-            var thisMove = Move.Left; //This and the co-ords above will start "1" at position 0,0
-            for (int i = 1; i <= arraySize; i++)
+            var gridSize = 347991;
+            var grid = new Dictionary<string, int>();
+            var moveOutcome = new MoveOutcome
             {
-                thisMove = GetNextDirection(thisMove);
-                currentCoordinates = GetNextCoordinates(currentCoordinates, thisMove);
-                _partOneGrid.Add(currentCoordinates, i);
+                X = 0,
+                Y = 0,
+                Direction = Move.First
+            };
+
+            for (int i = 1; i <= gridSize; i++)
+            {
+                moveOutcome = GetNextCoordinates(moveOutcome, grid);
+                grid.Add(GetCoordinate(moveOutcome.X, moveOutcome.Y), i);
             }
-            var locationOfTarget = _partOneGrid.FirstOrDefault(v => v.Value == arraySize).Key;
-            var locationOfStart = _partOneGrid.FirstOrDefault(v => v.Value == 1).Key;
+            var locationOfTarget = grid.FirstOrDefault(v => v.Value == gridSize).Key;
+            var locationOfStart = grid.FirstOrDefault(v => v.Value == 1).Key;
             Console.WriteLine($"Location Of Target: {locationOfTarget}");
             Console.WriteLine($"Location Of Start: {locationOfStart}");
             var targetX = int.Parse(locationOfTarget.Split(",")[0]);
             var targetY = int.Parse(locationOfTarget.Split(",")[1]);
             var startX = int.Parse(locationOfStart.Split(",")[0]);
             var startY = int.Parse(locationOfStart.Split(",")[1]);
-            var stepsAcross = targetX - startX;
-            var stepsUpDown = targetY - startY;
+            var stepsAcross = Math.Abs(targetX - startX);
+            var stepsUpDown = Math.Abs(targetY - startY);
             Console.WriteLine($"Steps Across: {stepsAcross}");
             Console.WriteLine($"Steps Up/Down: {stepsUpDown}");
-            Console.WriteLine($"Total Steps: {stepsAcross + stepsUpDown}");
+            Console.WriteLine($"Total Steps: {(stepsAcross + stepsUpDown)}");
         }
 
-        private static string GetNextCoordinates(string currentCoordinates, Move thisMove)
+        private static MoveOutcome GetNextCoordinates(MoveOutcome lastMove, Dictionary<string, int> grid)
         {
-            var currentX = int.Parse(currentCoordinates.Split(",")[0]);
-            var currentY = int.Parse(currentCoordinates.Split(",")[1]);
+            var currentX = lastMove.X;
+            var currentY = lastMove.Y;
 
-            if (_partOneGrid.ContainsKey(GetCoordinate(++currentX, currentY)))
-
-            switch (thisMove)
+            switch (lastMove.Direction)
             {
+                case Move.First: //Special case for first move.
+                    return new MoveOutcome
+                    {
+                        Direction = Move.Right,
+                    };
+
                 case Move.Right:
                     while (true)
                     {
-                        if (!_partOneGrid.ContainsKey(GetCoordinate(++currentX, currentY)))
+                        return new MoveOutcome
                         {
-                            return GetCoordinate(currentX, currentY);
-                        }
+                            Direction = GetNextDirection(currentX + 1, currentY, lastMove.Direction, grid),
+                            X = currentX + 1,
+                            Y = currentY
+                        };
                     }
+
                 case Move.Up:
                     while (true)
                     {
-                        if (!_partOneGrid.ContainsKey(GetCoordinate(currentX, ++currentY)))
+                        return new MoveOutcome
                         {
-                            return GetCoordinate(currentX, currentY);
-                        }
-
+                            Direction = GetNextDirection(currentX, currentY + 1, lastMove.Direction, grid),
+                            X = currentX,
+                            Y = currentY + 1
+                        };
                     }
                 case Move.Left:
                     while (true)
                     {
-                        if (!_partOneGrid.ContainsKey(GetCoordinate(--currentX, currentY)))
+                        return new MoveOutcome
                         {
-                            return GetCoordinate(currentX, currentY);
-                        }
+                            Direction = GetNextDirection(currentX - 1, currentY, lastMove.Direction, grid),
+                            X = currentX - 1,
+                            Y = currentY
+                        };
                     }
                 case Move.Down:
                     while (true)
                     {
-                        if (!_partOneGrid.ContainsKey(GetCoordinate(currentX, --currentY)))
+                        return new MoveOutcome
                         {
-                            return GetCoordinate(currentX, currentY);
-                        }
+                            Direction = GetNextDirection(currentX, currentY - 1, lastMove.Direction, grid),
+                            X = currentX,
+                            Y = currentY - 1
+                        };
                     }
             }
             throw new Exception("Invalid Input");
         }
 
-    private static string GetCoordinate(int x, int y) => x + "," + y;
-    
-
-    private static Move GetNextDirection(Move lastMove)
+        private static Move GetNextDirection(int x, int y, Move move, Dictionary<string, int> grid)
         {
-            switch (lastMove)
+            switch (move)
             {
-                case Move.Right:
-                    return Move.Up;
-                case Move.Up:
-                    return Move.Left;
-                case Move.Left:
-                    return Move.Down;
-                case Move.Down:
+                case Move.Right: //Peek at the one above to see if we should switch to Up
+                    if (!grid.ContainsKey(GetCoordinate(x, y + 1)))
+                    {
+                        return Move.Up;
+                    }
                     return Move.Right;
+                case Move.Up: //Peek at the one Left to see if we should switch to Left
+                    if (!grid.ContainsKey(GetCoordinate(x - 1, y)))
+                    {
+                        return Move.Left;
+                    }
+                    return Move.Up;
+                case Move.Left: //Peek at the one below to see if we should switch to Down
+                    if (!grid.ContainsKey(GetCoordinate(x, y - 1)))
+                    {
+                        return Move.Down;
+                    }
+                    return Move.Left;
+                case Move.Down: //Peek at the one to the right to see if we should switch to Right
+                    if (!grid.ContainsKey(GetCoordinate(x + 1, y)))
+                    {
+                        return Move.Right;
+                    }
+                    return Move.Down;
             }
-            throw new Exception("Invalid input");
+            throw new Exception("Invalid Input");
         }
-    }
 
-    internal enum Move
-    {
-        Right,
-        Up,
-        Left,
-        Down
+        
+        private static string GetCoordinate(int x, int y) => x + "," + y;
+
+        internal enum Move
+        {
+            Right,
+            Up,
+            Left,
+            Down,
+            First,            
+        }
+
+        internal class MoveOutcome
+        {
+            public int X;
+            public int Y;
+            public Move Direction;
+        }
     }
 }
--- AoC2017/Program.cs
+++ AoC2017/Program.cs
@@ -6,10 +6,10 @@
         {
             //Day1.Run();
             //Day2.Run();
-            //Day3.Run();
+            Day3.Run();
             //Day4.Run();
             //Day5.Run();
-            Day6.Run();
+            //Day6.Run();
         }
     }
 }
